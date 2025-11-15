a670dc2efacff38a9de23466814095eaaff803c9
Adil Iqbal <aiqbal@hq.bill.com>
2025-11-14 23:26:20 -0600

    win/macos locator throws correct error

--- src/locator/win.zig
+++ src/locator/win.zig
@@ -7,71 +7,71 @@ const Allocator = std.mem.Allocator;
 const Self = @This();
 
 pub fn getUserHomeOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserDataOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getSiteDataOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserConfigOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getSiteConfigOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserCacheOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getSiteCacheOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserStateOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserLogOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserDocumentsOwned(_: *const Self, _: Allocator) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserPicturesOwned(_: *const Self, _: Allocator) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserVideosOwned(_: *const Self, _: Allocator) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserMusicOwned(_: *const Self, _: Allocator) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserDesktopOwned(_: *const Self, _: Allocator) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getUserRuntimeOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn getSiteRuntimeOwned(_: *const Self, _: Allocator, _: *const Options) DirsError![]const u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 pub fn pathSeperator(_: *const Self) u8 {
-    return DirsError.UnsupportedOSError;
+    return DirsError.UnsupportedOperationError;
 }
 
 
