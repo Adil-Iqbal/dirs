543da8fea9e310878edb9019851e6c89b12c9976
Adil Iqbal <aiqbal@hq.bill.com>
2025-11-24 16:40:23 -0600

    fix top-level locator calls.

--- src/root.zig
+++ src/root.zig
@@ -55,96 +55,96 @@ pub fn getUserHomeOwned(alloc: Allocator) ![]const u8 {
 /// that the user wouldn't modify but would reasonably keep or back-up. Caller
 /// is responsible for freeing the returned value.
 pub fn getUserDataOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.getUserDataOwned(o, alloc);
+    return try Self.locator.getUserDataOwned(alloc, o);
 }
 
 /// Standard directory for storage of user-owned and application-specific files
 /// that the users wouldn't modify but would reasonably keep or back-up.
 /// Available to all users.
 pub fn getSiteDataOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.getSiteDataOwned(o, alloc);
+    return try Self.locator.getSiteDataOwned(alloc, o);
 }
 
 /// Standard directory for storage of user-specific configuration files.
 /// Consider for editable files that customize the behavior of the application.
 pub fn getUserConfigOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedUserConfig(o, alloc);
+    return try Self.locator.getUserConfigOwned(alloc, o);
 }
 
 /// Standard directory for storage of configuration files needed by all users.
 /// Consider for editable files that customize the behavior of the application.
 pub fn getSiteConfigOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedSiteConfig(o, alloc);
+    return try Self.locator.getSiteConfigOwned(alloc, o);
 }
 
 /// Standard directory for storage of user specific cached data that can be 
 /// deleted at any time without loss of application functionality. 
 /// Consider for downloaded assets or compiled templates.
 pub fn getUserCacheOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedUserCache(o, alloc);
+    return try Self.locator.getUserCacheOwned(alloc, o);
 }
 
 /// Standard directory for storage of cached data for all users that can be 
 /// deleted at any time without loss of application functionality. 
 /// Consider for downloaded assets or compiled templates.
 pub fn getSiteCacheOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedSiteCache(o, alloc);
+    return try Self.locator.getSiteCacheOwned(alloc, o);
 }
 
 /// Standard directory for storage of user specific files that represent
 /// persistent state that must survive application restart.
 /// Consider for files that represent the data layer of the application.
 pub fn getUserStateOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedUserState(o, alloc);
+    return try Self.locator.getUserStateOwned(alloc, o);
 }
 
 /// Standard directory for storage of user specific log files.
 /// Consider for files that can be used to audit application behavior.
 pub fn getUserLogOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedUserLog(o, alloc);
+    return try Self.locator.getUserLogOwned(alloc, o);
 }
 
 /// Standard directory for storage of user-owned files that are
 /// application-agnostic. Consider for files that the user would reasonably be
 /// expcted to use outside the context of your application, such as exports.
 pub fn getUserDocumentsOwned(alloc: Allocator) DirsError![]const u8 {
-    return try Self.locator.ownedUserDocuments(alloc);
+    return try Self.locator.getUserDocumentsOwned(alloc);
 }
 
 /// Standard directory for storage of user-specific image files.
 /// Consider for file formats: .jpg, .gif, .png, .tiff
 pub fn getUserPicturesOwned(alloc: Allocator) DirsError![]const u8 {
-    return try Self.locator.ownedUserPictures(alloc);
+    return try Self.locator.getUserPicturesOwned(alloc);
 }
 
 /// Standard directory for storage of user-specific video files.
 /// Consider for file formats: .mp4, .mov, .wmv
 pub fn getUserVideosOwned(alloc: Allocator) DirsError![]const u8 {
-    return try Self.locator.ownedUserVideos(alloc);
+    return try Self.locator.getUserVideosOwned(alloc);
 }
 
 /// Standard directory for storage of user-specific audio files. 
 /// Consider for file formats: .wav, .mp3, .midi
 pub fn getUserMusicOwned(alloc: Allocator) DirsError![]const u8 {
-    return try Self.locator.ownedUserMusic(alloc);
+    return try Self.locator.getUserMusicOwned(alloc);
 }
 
 /// Standard directory that renders file contents on the user's 
 /// Desktop. Consider for shortcuts and symlinks to your application.
 pub fn getUserDesktopOwned(alloc: Allocator) DirsError![]const u8 {
-    return try Self.locator.ownedUserDesktop(alloc);
+    return try Self.locator.getUserDesktopOwned(alloc);
 }
 
 /// Standard directory for storage of user specific temporary files that
 /// support application runtime.
 pub fn getUserRuntimeOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedUserRuntime(o, alloc);
+    return try Self.locator.getUserRuntimeOwned(alloc, o);
 }
 
 /// Standard directory for storage of temporary files that support application
 /// runtime for all users.
 pub fn getSiteRuntimeOwned(alloc: Allocator, o: *const Options) DirsError![]const u8 {
-    return try Self.locator.ownedSiteRuntime(o, alloc);
+    return try Self.locator.getSiteRuntimeOwned(alloc, o);
 }
 
 pub const isMultipath = util.isMultipath;
